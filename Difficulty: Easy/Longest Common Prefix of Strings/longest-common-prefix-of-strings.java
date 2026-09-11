ce714acbbe31d30cc2f430a84f8096dd0a600919
shreyaashuklaa <shreyashuklak91@gmail.com>
2026-09-11 12:34:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String longestCommonPrefix(String strs[]) {
+        // code here
+        int minLen=strs[0].length();
+        for(String str: strs){
+            minLen=Math.min(minLen, str.length());
+        }
+        int i=0;
+        while(i<minLen){
+            int j=0;
+            while(j<strs.length-1){
+                if(strs[j].charAt(i)!=strs[j+1].charAt(i)){
+                    return strs[0].substring(0,i);
+                }
+                j=j+1;
+            }
+            i=i+1;
+        }
+        return strs[0].substring(0,i);
+    }
+}
